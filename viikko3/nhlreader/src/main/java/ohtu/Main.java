e9f7aeae70597c5128015fac2bd5dfb3848d9926
Henri Silvan <henri.silvan@helsinki.fi>
2020-03-30 23:58:59 +0300

    lisätty nhlreader

---
+++ viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.Date;
+import org.apache.http.client.fluent.Request;
+
+/**
+ *
+ * @author mluukkai
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
+
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+
+        Gson mapper = new Gson();
+        Player[] players = mapper.fromJson(bodyText, Player[].class);
+
+        System.out.println("Players from FIN " + new Date());
+        System.out.println("");
+        
+        for (Player player : players) {
+            if (player.getNationality().equals("FIN")) {
+//                System.out.println(player.getName() + " team " + player.getTeam() + " goals " + player.getGoals() + " assists " + player.getAssists());
+                System.out.format("%-20s%-5s%2d + %2d = %2d\n", player.getName(), player.getTeam(), player.getGoals(), player.getAssists(), player.getGoals()+player.getAssists());
+            }
+        }
+    }
+}
---
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -0,0 +1,56 @@
+
+package ohtu;
+
+public class Player {
+    private String name;
+    private String team;
+    private String nationality;
+    private int goals;
+    private int assists;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+      
+}
